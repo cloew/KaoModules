6dd2ae78114311decb79cba8ca797daafccb2165
Chris Loew <cloew123@gmail.com>
2015-03-18 14:47:26 -0600

    Added a helper class to interact with a class given its python package path

--- src/kao_modules/__init__.py
+++ src/kao_modules/__init__.py
@@ -1,1 +1,2 @@
 from .kao_module import KaoModule
+from .namespaced_class import NamespacedClass
---
+++ src/kao_modules/namespaced_class.py
@@ -0,0 +1,18 @@
+from .kao_module import KaoModule
+
+class NamespacedClass:
+    """ Represents a class that is specfied as $module.$class """
+    
+    def __init__(self, namespaceString):
+        """ Initialize the namespaced class with the namespace """
+        moduleName, self.className = namespaceString.rsplit('.', 1)
+        self.module = KaoModule(moduleName)
+        
+    def instantiate(self, *args, **kwargs):
+        """ Instantiate this class """
+        return self.cls(*args, **kwargs)
+        
+    @property
+    def cls(self):
+        """ Return the underlying Python class this wraps """
+        return getattr(self.module.module, self.classname)
